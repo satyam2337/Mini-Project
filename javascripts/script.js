cf52d920020fc8f305aa750e055b071e576db581
satyam <satyamraj078@gmail.com>
2025-01-30 19:12:54 +0530

    dd

--- javascripts/script.js
+++ javascripts/script.js
@@ -21,7 +21,7 @@ async function generateImage() {
     "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
     {
       headers: {
-        Authorization: "Bearer apikey",
+        Authorization: "Bearer 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",
         "Content-Type": "application/json",
       },
       method: "POST",
